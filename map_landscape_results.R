d69b976d81a54cad77bfef666af698467b7cc145
mrke <mrke@unimelb.edu.au>
2015-06-11 17:30:43 +1000

    fixed bugs in mapping file

--- map_landscape_results.R
+++ map_landscape_results.R
@@ -1,7 +1,7 @@
 library(raster)
 library(ncdf)
 
-setwd('landscape_sims/')
+setwd('c:/NicheMapR_Working/projects/bearded dragons/landscape sims/')
 longlats<-read.csv('longlats.csv')[,2:3]
 files<-list.files()
 files1997<-files[grep(files,pattern = '1997')]
@@ -21,7 +21,7 @@ quadwid<-(lon2-lon1)/quadres
 quadlen<-(lat2-lat1)/quadres
 gridout <- raster(ncol=quadwid, nrow=quadlen, xmn=lon1, xmx=lon2, ymn=lat1, ymx=lat2)
 
-x<-cbind(longlats[,1],longlats[,2])
+
 
 sims<-c("var72_85","con72","con78","con85")
 years<-seq(1997,2001,1)
@@ -31,11 +31,12 @@ for(m in 1:length(years)){
 dates<-seq(ISOdate(years[m],1,1,tz=tzone)-3600*12, ISOdate(years[m]+1,1,1,tz=tzone)-3600*13, by="days")
 dates<-subset(dates, format(dates, "%m/%d")!= "02/29") # remove leap years
 for(i in 1:eval(parse(text=paste("length(files",years[m],")",sep='')))){
-  data<-read.csv(eval(parse(text=paste("files",years[m],"[1]",sep=""))),header=FALSE)
+  data<-read.csv(eval(parse(text=paste("files",years[m],"[",i,"]",sep=""))),header=FALSE)
   colnames(data)<-colnames
   data[,5]<-data[,5]-data[1,5]+1 # get correct day of year
   for(j in 1:365){
   daily_sub<-subset(data,doy==j)
+  x<-cbind(daily_sub[,2],daily_sub[,3])
   grid <- rasterize(x, gridout, daily_sub$maxrun)
   #plot(grid,main=j)
   if(j==1){s<-grid}else{s<-stack(s,grid)}
